ec98a05f18f94f3894c1961cf4ff7d9df2632713
Eider Prada <eiderprada2914@gmail.com>
2024-05-28 11:03:11 -0500

    ni_idea

--- view/social_media.php
+++ view/social_media.php
@@ -11,7 +11,7 @@ if(isset($_SESSION["id"])){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Inicio</title>
+  <title>view_social_media</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -125,62 +125,60 @@ if(isset($_SESSION["id"])){
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+    <div class="contenido-susan">
+        <?php
+            $proyecto = "Admón de Redes Sociales";
+            ?>
 
-    <!-- Main content -->
-    <section class="content">
-<!-- Default box -->
-        <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Title</h3>
-
-            <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                <i class="fas fa-times"></i>
-            </button>
-            </div>
-        </div>
-        <div class="card-body">
-            Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-            Footer
-        </div>
-        <!-- /.card-footer-->
-        </div>
-<!-- /.card -->
-
-</section>
-    <!-- /.content -->
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title><?php echo $proyecto; ?></title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <style>
+                    .contenido-susan {
+                        background-color: #fff;
+                        border-radius: 8px;
+                        margin-left: 20px;
+                        margin-right: 20px;
+                    }
+                    .btn-create {
+                        border-color: blue; /* Borde azul */
+                        color: blue; /* Texto azul */
+                    }
+                    .btn-load {
+                        border-color: gray; /* Borde gris */
+                        color: gray; /* Texto gris */
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="col-lg-8 mx-auto p-3 py-md-5">
+                    <header class="d-flex align-items-center pb-3 mb-5 border-bottom">
+                        <a href="#" class="d-flex align-items-center text-dark text-decoration-none">
+                            <span class="fs-4"><?php echo $proyecto; ?></span>
+                        </a>
+                    </header>
+                    <div class="mt-3">
+                            <a href="process.php?regenerar=1" class="btn btn-create">crear</a>
+                            <a href="reset.php" class="btn btn-load">cargar</a>
+                    </div>
+                </div>
+            </body>
+            </html>
+    </div>
   </div>
-  <!-- /.content-wrapper -->
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.1.0
     </div>
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+  </foote
 
+  <!-- /.content-wrapper -->
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
